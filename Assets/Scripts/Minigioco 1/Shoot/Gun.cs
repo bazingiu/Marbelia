152f387f10a7fa61e6961eddc67bd8bbd80de355
Bazingiu <giulita00@gmail.com>
2022-05-03 16:58:03 +0200

    Aggiunta mappa della citta

---
+++ Assets/Scripts/Minigioco 1/Shoot/Gun.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f; 
+    
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Quando si preme il tasto destro per sparare
+        if(Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+    }
+
+    //sparo con raycast, si fa il controllo se si prende qualcosa o meno
+    void shoot()
+    {
+        RaycastHit hit; 
+        //controlliamo se abbiamo preso qualcosa sparando
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
