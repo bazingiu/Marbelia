70fc8d8253692e37c565ab67b3f519a483ce32f1
Bazingiu <giulita00@gmail.com>
2022-05-03 18:12:48 +0200

    Sparo con le particelle

--- Assets/Scripts/Minigioco 1/Shoot/Gun.cs
+++ Assets/Scripts/Minigioco 1/Shoot/Gun.cs
@@ -6,6 +6,7 @@ public class Gun : MonoBehaviour
     public float range = 100f; 
     
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
 
     // Update is called once per frame
     void Update()
@@ -20,11 +21,19 @@ public class Gun : MonoBehaviour
     //sparo con raycast, si fa il controllo se si prende qualcosa o meno
     void shoot()
     {
+        muzzleFlash.Play();
+        
         RaycastHit hit; 
         //controlliamo se abbiamo preso qualcosa sparando
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
         }
     }
 }
