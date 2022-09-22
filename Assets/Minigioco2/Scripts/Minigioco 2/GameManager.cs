41f8ad0754f13f02542f1082a31ec030db5316d4
Giovanni Maugeri <maugerigiovanni31@gmail.com>
2022-09-22 15:11:02 +0200

    add script minigioco 2

---
+++ Assets/Minigioco2/Scripts/Minigioco 2/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameOverScreen GameOverScreen;
+
+    bool pause = false;
+
+    public void GameOver(){
+        GameOverScreen.Setup();
+    }
+
+    public void mainMenu(){
+        pause = !pause;
+        if (pause == false)
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    void Start()
+    {
+        Time.timeScale = 1;   
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Minigioco2/Scripts/Minigioco 2/Player/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minigioco1
+
+public class Player : MonoBehaviour{
+
+    [SerializeField] protected float maxHealth;
+    [SerializeField] protected float speed;
+
+    public bool invicible = false;
+    public float health;
+    [SerializeField] public int points = 0;
+
+    public GameManager game;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        Init();
+    }
+
+    public float getHealth()
+    {
+        return health;
+    }
+
+    private void Init(){
+        health = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape")){
+            game.mainMenu();
+        }
+    }
+
+    public void Hit (float damage){
+        if(invicible==false){
+            health -= damage;
+            healthBar.SetHealth(health);
+        }
+        if (health<=0){
+            Time.timeScale = 0;
+            Kill();
+        }
+    }
+
+    public void Kill(){
+        gameObject.SetActive(false);
+        game.GameOver();
+    }
+
+    public void addPoints(int newPoints){
+        points += newPoints;
+    }
+}
